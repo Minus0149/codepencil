49c987089b8e3d642f231d0c8b545e9b468df2c1
Minus0149 <minus.official.01@gmail.com>
2023-03-09 20:04:49 +0530

    Added close and open fuctionality to the editors

--- src/app/Editor.js
+++ src/app/Editor.js
@@ -7,17 +7,25 @@ import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/css/css';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
+import { FaCompressAlt, FaExpandAlt } from 'react-icons/fa';
 
 const Editor = (props) => {
   const { displayName, language, value, onChange } = props;
+  const [open, setOpen] = useState(true);
   const handleChange = (editor, data, value) => {
     onChange(value);
   };
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
       <div className="editor-title">
         {displayName}
-        <button> O/C</button>
+        <button
+          type="button"
+          className="expand-collapse-button"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <FaCompressAlt /> : <FaExpandAlt />}
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
